c3e02386444262e1e1c069515a39e8e17165b9d4
Railson25 <railsonsantiago123@gmail.com>
2022-03-27 17:42:28 -0300

    Header

--- project/app/js/script.js
+++ project/app/js/script.js
@@ -1,1 +1,19 @@
-console.log("olá")
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const fadeElements = document.querySelector('.has-fade')
+
+btnHamburger.addEventListener('click', function() {
+    console.log('open')
+
+    if(btnHamburger.classList.contains('open')) { // Close menu
+        body.classList.remove('noscroll')
+        btnHamburger.classList.remove('open')
+        fadeElements.classList.add('has-fade')
+        
+    }else { // Open menu
+        body.classList.add('noscroll')
+        btnHamburger.classList.add('open')
+        fadeElements.classList.remove('has-fade')
+    }
+    
+})
